dfd33057c5e2d8c51343bb963c00cb84825f7a9d
strrules105 <camobot3@gmail.com>
2021-09-28 10:39:58 -0400

    Initialized React App and added api osrs-json-hiscores and osrs-json-api. No idea how to use these

---
+++ app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import hiscores, {getSkillPage} from 'osrs-json-hiscores';
+
+const stats = await hiscores.getStats('str rules105');
+const topPage = await getSkillPage('overall');
+
+const App: React.FunctionComponent = () => {
+  return(
+    <div>
+      {stats}
+    </div>
+  );
+};
+export default App;
