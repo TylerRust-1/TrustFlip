830cc17ecd3dd6527da058dd295d01631bcf3093
strrules105 <camobot3@gmail.com>
2021-09-30 19:05:47 -0400

    Implemented a new background, trying to center content and figure out how to display account information.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,20 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import hiscores, {getSkillPage, Player} from 'osrs-json-hiscores';
+import './index.css';
+//import hiscores, {getSkillPage, Player} from 'osrs-json-hiscores';
+import { Hiscores, News, Player } from 'oldschooljs';
+import { isWhiteSpaceLike } from 'typescript';
+import { AccountType } from 'oldschooljs/dist/meta/types';
+//import { AccountType } from 'oldschooljs/dist/meta/types';
 
 
+/* OSRS JSON HISCORES API || COULDN'T GET WORKING */
 //const stats = hiscores.getStats('str rules105');
 //const skillPage = getSkillPage('overall');
 
-const Top: React.FunctionComponent = () => {
-  return(
-    <div>
-      <h1>TrustFlip</h1>
-      <h2><a href ="https://github.com/strrules105/TrustFlip">Github Repository</a></h2>
-    </div>
-  )
-}
-
+/*
 function PlayerView():JSX.Element{
   const [data, setData] = useState<Player>()
 
@@ -32,12 +30,36 @@ function PlayerView():JSX.Element{
     </div>
   )
 }
+*/
 
-const Skills: React.FunctionComponent = () => {
+function PlayerView ():JSX.Element{
+  const [data, setData] = useState<Player>()
+
+  useEffect( () => {
+    async function test (){
+      const str  = await Hiscores.fetch('str rules105', {type: AccountType.Normal, virtualLevels:true});
+      setData(str);
+      const recentArticles = await News.fetchRecent();
+      const mostRecentArticle = recentArticles[0];
+    }
+    test()
+  },[])
   return(
-    <React.Fragment>
+    <div style={styles.container}>
+      {data === null ?
+        <span>Loading</span> :
+        <span>{data}</span>}
+    </div>
+  )
+}
+
+const Top: React.FunctionComponent = () => {
+  return(
+    <div style = {styles.container}>
+      <h1>TrustFlip</h1>
+      <h2><a href ="https://github.com/strrules105/TrustFlip">Github Repository</a></h2>
       {PlayerView}
-    </React.Fragment>
+    </div>
   )
 }
 
@@ -45,8 +67,17 @@ const App: React.FunctionComponent = () => {
   return(
     <div>
       <Top />
-      <Skills />
     </div>
   );
 };
+
+const styles = {
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: 'rgb(122, 121, 120)',
+    width: "60%",
+    color: 'black',    
+  } 
+}
 export default App;
