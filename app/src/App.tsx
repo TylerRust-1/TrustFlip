614e7764231a866ee3d7565b2eb4733b21111fc1
strrules105 <camobot3@gmail.com>
2021-09-30 10:11:38 -0400

    Was able to define stats and SkillPage from the API, now to get them to render.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,13 +2,37 @@ import React from 'react';
 import './App.css';
 import hiscores, {getSkillPage} from 'osrs-json-hiscores';
 
-const stats = await hiscores.getStats('str rules105');
-const topPage = await getSkillPage('overall');
+
+const stats = hiscores.getStats('str rules105');
+const skillPage = getSkillPage('overall');
+
+//const topPage = await getSkillPage('overall');
+
+const Top: React.FunctionComponent = () => {
+  return(
+    <div>
+      <h1>TrustFlip</h1>
+      <h2><a href ="https://github.com/strrules105/TrustFlip">Github Repository</a></h2>
+      <div>
+        getStats;
+      </div>
+    </div>
+  )
+}
+
+const Skills: React.FunctionComponent = () => {
+  return(
+    <React.Fragment>
+      {stats}
+    </React.Fragment>
+  )
+}
 
 const App: React.FunctionComponent = () => {
   return(
     <div>
-      {topPage}
+      <Top />
+      <Skills />
     </div>
   );
 };
